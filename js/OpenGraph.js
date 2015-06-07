3ff7bdded7f8b1b1dd9226d605f5d2909e4e3f21
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2015-06-07 00:00:17 -0700

    Mobile Debugging Take 11

--- js/OpenGraph.js
+++ js/OpenGraph.js
@@ -7,7 +7,7 @@ $(document).ready(function()
 	$(window).resize(detectMobile);
 	if(location.hash!= "")
 		updateEntry($(".entry")[0], location.hash.substring(1));
-	changeHeader(navigator.platform);
+	changeHeader(navigator.appVersion);
 });
 
 // Writes into the given entry with the given string
