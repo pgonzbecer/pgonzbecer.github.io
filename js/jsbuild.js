45f1c5688652702d8c0bf9d1cf0651659d97d38c
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2015-08-17 12:28:44 -0700

    Update jsbuild.js

--- js/jsbuild.js
+++ js/jsbuild.js
@@ -3,6 +3,27 @@
 build=  function(args)
 {
   console.log(args);
+  if(!args.target || $(args.target)== null)
+    throw exception("No target located")
+  
+  // Variables
+  var target= getContent(args.target);
+  
+  if(args.innerHTML)
+  {
+    // Variables
+    var ih= getContent(args.innerHTML);
+    
+    target.html(ih);
+  }
+  
+  function getContent(argument)
+  {
+    if(argument== null)
+      throw exception("Argument is null");
+    
+    return (($.isFunction(argument) ? argument(arguments) : argument))
+  }
 };
 
 // End of File
