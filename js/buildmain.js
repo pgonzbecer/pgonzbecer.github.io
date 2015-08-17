c54df5688602bd35cd328a5edec647e3c13e69cb
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2015-08-17 12:16:58 -0700

    Create buildmain.js

---
+++ js/buildmain.js
@@ -0,0 +1,18 @@
+// Created by Paul Gonzalez Becerra
+
+$(document).ready(function(args)
+{
+  $.build({
+    target: ".insert-nav",
+    innerHTML:  getNavigation
+  });
+});
+
+function getNavigation(args)
+{
+  return (
+    "DERP"
+  );
+};
+
+// End of File
