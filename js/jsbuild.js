b66e15008430ad2a3e69c47389449b756c10afa2
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2015-08-19 09:30:01 -0700

    update

--- js/jsbuild.js
+++ js/jsbuild.js
@@ -1,7 +1,41 @@
 // Created by Paul Gonzalez Becerra
 
-build=  function(args)
+/*
+	appendHead: [HeadAppendObject]
+	
+	HeadAppendObject	{
+		tagName = string,
+		#if(tagName is link)
+			rel = string,
+			type = string,
+			href = string
+		#endif
+	}
+*/
+
+build=	function(args)
 {
+	if(args.appendHead)
+	{
+		// Variables
+		var	node;
+		
+		for(var i= 0; i< args.appendHead.length; i++)
+		{
+			switch(args.appendHead[i].tagName)
+			{
+				case "link":
+					node=	document.createElement("link");
+					node.setAttribute("rel", args.appendHead[i].rel);
+					node.setAttribute("type", args.appendHead[i].type);
+					node.setAttribute("href", args.appendHead[i].href);
+					break;
+			}
+		}
+		document.head.appendChild(node);
+	}
+	
+	
   console.log(args);
   if(!args.target || $(args.target)== null)
     throw exception("No target located")
--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Created by Paul Gonzalez Becerra
-
-function sendTo(weburl)
-{
-	location.assign(weburl);
-}
-
-// End of File
