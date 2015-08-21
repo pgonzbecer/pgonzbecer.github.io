e395d983d75689937231829ca2781a2a7ac5709b
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2015-08-21 11:46:41 -0700

    jimmy jams

--- js/docsbuilder.js
+++ js/docsbuilder.js
@@ -50,7 +50,7 @@ function buildDocumentation(json)
 	// Variables
 	var	idoc=	$(".doc-interface");
 	var str;
-	var	colordif=	true;
+	var	colordif;
 	
 	idoc.html("");
 	idoc.append("<span class='index-title'>"+json.title+"</span>");
@@ -58,6 +58,7 @@ function buildDocumentation(json)
 	
 	for(var h= 0; h< json.tables.length; h++)
 	{
+		colordif=	true;
 		str=	"<div>";
 		str+=	"<span class='title'>"+json.tables[h].title+"</span>";
 		str+=	"<div class='row'>";
@@ -65,9 +66,9 @@ function buildDocumentation(json)
 		switch(json.tables[h].type.toLowerCase())
 		{
 			case "namespace":
-				str+=	"<div class='col-xs-1'></div>";
-				str+=	"<div class='col-xs-5' style='text-align: right;'>Namespaces</div>";
-				str+=	"<div class='col-xs-6'>Description</div>";
+				str+=	"<div class='col-xs-1 col-header'></div>";
+				str+=	"<div class='col-xs-5 col-header' style='text-align: right;'>Namespaces</div>";
+				str+=	"<div class='col-xs-6 col-header'>Description</div>";
 				str+=	"</div>"; // Closes row
 				for(var k= 0; k< json.tables[h].content.length; k++)
 				{
@@ -80,8 +81,23 @@ function buildDocumentation(json)
 					str+=	"</div>";
 				}
 				break;
-			case "nested-objects":
+			case "classes":
+				str+=	"<div class='col-xs-1 col-header'></div>";
+				str+=	"<div class='col-xs-5 col-header'></div>";
+				str+=	"<div class='col-xs-6 col-header'></div>";
 				str+=	"</div>"; // Closes row
+				for(var k= 0; k< json.tables[h].content.length; k++)
+				{
+					str+=	"<div class='row color-"+((colordif) ? "light" : "dark")+"'>";
+					colordif=	!colordif;
+					str+=	"<div class='col-xs-1'>";
+					if(json.tables[h].content[k].remarks!= "")
+						str+=	"<a href='"+json.tables[h].content[k].remarksAnchor+"<span class='glyphicon glyphicon-list-alt'></span></a>";
+					str+=	"</div>";
+					str+=	"<div class='col-xs-5 class-link'><a href='?docname="+format(json.tables[h].content[k].href)+"'>"+
+						json.tables[h].content[k].text+"</a></div>";
+					str+=	"<div class='col-xs-6'>"+json.tables[h].content[k].desc+"</div>";
+				}
 				break;
 		}
 	}
