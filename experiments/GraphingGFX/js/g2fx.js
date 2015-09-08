262bdf69125e9a4e31ea49258175bbda85b09296
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2015-09-08 15:52:20 -0700

    Update g2fx.js

--- experiments/GraphingGFX/js/g2fx.js
+++ experiments/GraphingGFX/js/g2fx.js
@@ -3,4 +3,21 @@
 // Variables
 var	g2fx=	{};
 
+g2fx.create=  function(type, id, options)
+{
+	// Variables
+	var	elem=	document.getElementById(id);
+	
+	switch(type.toLowerCase())
+	{
+		case "graph":	return new Graph(elem, options);
+		default:	throw exception("Type [ "+type+" ] is unknown");
+	}
+	
+	function Graph(tElem, options)
+	{
+		
+	}
+};
+
 // End of File
