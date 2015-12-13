726cca92f8022af8c9f10a0bec835313790522b0
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2015-12-12 23:29:53 -0800

    rehash

---
+++ js/tabs.js
@@ -0,0 +1,27 @@
+// Created by Paul Gonzalez Becerra
+
+window.addEventListener("load", function()	{
+	// Variables
+	var	ctabs=	document.getElementById("content-tabs").getElementsByClassName("tab");
+	
+	for(var i= 0; i< ctabs.length; i++)
+	{
+		ctabs[i].onclick=	onClickTab;
+	}
+	
+	function onClickTab(){
+		// Variables
+		var	ct=	document.getElementsByClassName("tabcontent");
+		var	index=	-1;
+		
+		for(var i= 0; i< ct.length; i++)
+		{
+			if(ct[i].getAttribute("name")== this.getAttribute("name"))
+				index=	i;
+			ct[i].setAttribute("class", "tabcontent");
+		}
+		ct[index].setAttribute("class", "tabcontent visible");
+	}
+});
+
+// End of File
